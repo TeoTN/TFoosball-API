ca250de5d5290989c954dff1e20390d7684d84d9
Piotr Staniów <staniowp@gmail.com>
2016-10-23 19:46:54 +0200

    Adjust ExpHistory model admin page

--- tfoosball/tfoosball/admin.py
+++ tfoosball/tfoosball/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import Player, Match, ExpHistory
 
+
+class ExpHistoryAdmin(admin.ModelAdmin):
+    list_display = ('username', 'date', 'exp',)
+    
+    def username(self, obj):
+        return obj.player.username
+
 admin.site.register(Player)
 admin.site.register(Match)
-admin.site.register(ExpHistory)
+admin.site.register(ExpHistory, ExpHistoryAdmin)
