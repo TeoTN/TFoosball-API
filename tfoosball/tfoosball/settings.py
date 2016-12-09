aecb96048720943d29348b3b97472fef00cceed3
Piotr Staniów <staniowp@gmail.com>
2016-12-10 00:35:23 +0100

    Fix minor issue

--- tfoosball/tfoosball/settings.py
+++ tfoosball/tfoosball/settings.py
@@ -142,7 +142,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'tfoosball', 'static'),
+    os.path.join(BASE_DIR, '..', 'static'),
 ]
 SITE_ID = 1
 
