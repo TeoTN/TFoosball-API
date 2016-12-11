9e91e0b5e66889c67e6dd0fd14ff9e0dae8057e9
Piotr Staniów <staniowp@gmail.com>
2016-12-11 19:30:14 +0100

    Deployment (#5)
    * Clean templates and urls
    * Split settings to common, dev and production
    * Add psycopg2 as dependency
    * Set debug mode in line with environment variable
    * Add release step to Procfile.
    * Update Django. Limit db max conn age
    * Setup handling static files
    * Fix minor issue with static path
    * Fix minor issue: templates path

---
+++ tfoosball/dev_settings.py
@@ -0,0 +1,56 @@
+from tfoosball.common_settings import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '7d!g5l*3nm1=2s@&%11d+jz_$#ii2bugj+9ynhq&cfl0r%pnn)'
+DEBUG = True
+ALLOWED_HOSTS = []
+
+# DATABASE
+# ------------------------------------------------------------------------------
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# CORS
+# ------------------------------------------------------------------------------
+
+CORS_ORIGIN_WHITELIST = (
+    'localhost:8000',
+    'localhost:3000',
+    '127.0.0.1:3000',
+    '127.0.0.1:8000',
+)
+
+# LOGGING
+# ------------------------------------------------------------------------------
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'matches.log'),
+            'formatter': 'verbose'
+        },
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'tfoosball.matches': {
+            'handlers': ['file', 'console'],
+            'level': 'DEBUG',
+        }
+    },
+}
---
+++ tfoosball/production_settings.py
@@ -0,0 +1,29 @@
+from tfoosball.common_settings import *
+import os
+import dj_database_url
+
+
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+DEBUG = os.environ.get('DJANGO_DEBUG', False)
+ALLOWED_HOSTS = [
+    'tfoosball-api.herokuapp.com',
+    'tfoosball.herokuapp.com',
+]
+
+CSRF_TRUSTED_ORIGINS = ALLOWED_HOSTS
+
+DATABASES = {
+    'default': dj_database_url.config(conn_max_age=500)
+}
+
+# CORS
+# ------------------------------------------------------------------------------
+
+CORS_ORIGIN_WHITELIST = (
+    'tfoosball-api.herokuapp.com',
+    'tfoosball.herokuapp.com',
+)
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
