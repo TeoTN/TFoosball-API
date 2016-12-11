e7a080533d27145833b28332a13fe165241b87b5
Piotr Staniów <staniowp@gmail.com>
2016-12-11 01:17:18 +0100

    Fix minor issue with static path

--- tfoosball/common_settings.py
+++ tfoosball/common_settings.py
@@ -5,7 +5,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, '..', 'static')]
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 ROOT_URLCONF = 'tfoosball.urls'
 
