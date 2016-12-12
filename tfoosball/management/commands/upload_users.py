d796608b462c924a3938c97d32744c4064b3be07
oszka496 <oszka496@gmail.com>
2016-12-12 21:38:22 +0100

    Commands fixes

--- tfoosball/management/commands/upload_users.py
+++ tfoosball/management/commands/upload_users.py
@@ -1,15 +1,17 @@
 from django.core.management.base import BaseCommand
 from tfoosball.models import Player
+import users
+
 
 class Command(BaseCommand):
-	help = 'Populates db with users from tfoosball/fixtures/users.json'
+    help = 'Populates db with users from tfoosball/fixtures/users.json'
 
-	def upload_users():
-		with open('tfoosball/fixtures/users.json', 'r') as fh:
-		     users = json.load(fh)
+    def upload_users():
+        with open('tfoosball/fixtures/users.json', 'r') as fh:
+             users = json.load(fh)
 
-		for user in users:
-		    Player.objects.create_user(**user)
+        for user in users:
+            Player.objects.create_user(**user)
 
-	def handle(self, *args, **options):
-		self.upload_users()
+    def handle(self, *args, **options):
+        self.upload_users()
