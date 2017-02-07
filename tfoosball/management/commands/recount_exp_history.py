59e5c967b66c2327abe4d02efa095864e64ce032
Piotr Staniów <staniowp@gmail.com>
2017-02-07 20:09:41 +0100

    Update recount exp history script

--- tfoosball/management/commands/recount_exp_history.py
+++ tfoosball/management/commands/recount_exp_history.py
@@ -1,51 +1,51 @@
 from django.core.management.base import BaseCommand
-from tfoosball.models import Player, MatchLegacy, ExpHistoryLegacy
+from tfoosball.models import Match, ExpHistory, Member
 
 
 class Command(BaseCommand):
     help = 'Deletes exp history and creates it from scratch'
 
     def delete_history(self):
-        ExpHistoryLegacy.objects.all().delete()
+        ExpHistory.objects.all().delete()
 
     def init_history(self):
-        for player in Player.objects.all():
-            eh = ExpHistoryLegacy(player=player, exp=1000)
+        for member in Member.objects.all():
+            eh = ExpHistory(player=member, exp=1000)
             eh.save()
-            eh.date = player.date_joined
+            eh.date = member.date_joined
             eh.save()
-            player.exp = 1000
-            player.save()
+            member.exp = 1000
+            member.save()
 
     def create_history(self):
         def get_points(match, role):
             return match.points if role == 'red' else -match.points
 
-        for match in MatchLegacy.objects.all().order_by('date'):
+        for match in Match.objects.all().order_by('date'):
             match.red_att.exp += get_points(match, 'red')
             match.red_att.save()
-            eh = ExpHistoryLegacy(player=match.red_att, exp=match.red_att.exp, match=match)
+            eh = ExpHistory(player=match.red_att, exp=match.red_att.exp, match=match)
             eh.save()
             eh.date = match.date
             eh.save()
 
             match.red_def.exp += get_points(match, 'red')
             match.red_def.save()
-            eh = ExpHistoryLegacy(player=match.red_def, exp=match.red_def.exp, match=match)
+            eh = ExpHistory(player=match.red_def, exp=match.red_def.exp, match=match)
             eh.save()
             eh.date = match.date
             eh.save()
 
             match.blue_att.exp += get_points(match, 'blue')
             match.blue_att.save()
-            eh = ExpHistoryLegacy(player=match.blue_att, exp=match.blue_att.exp, match=match)
+            eh = ExpHistory(player=match.blue_att, exp=match.blue_att.exp, match=match)
             eh.save()
             eh.date = match.date
             eh.save()
 
             match.blue_def.exp += get_points(match, 'blue')
             match.blue_def.save()
-            eh = ExpHistoryLegacy(player=match.blue_def, exp=match.blue_def.exp, match=match)
+            eh = ExpHistory(player=match.blue_def, exp=match.blue_def.exp, match=match)
             eh.save()
             eh.date = match.date
             eh.save()
