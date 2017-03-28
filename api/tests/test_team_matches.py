03f20c529a7a626696e263b23ff9204a242bb8ad
oszka496 <oszka496@users.noreply.github.com>
2017-03-28 22:37:25 +0200

    Issues/failing tests (#15)
    * Fix fixtures + tests
    * Extract variable

--- api/tests/test_team_matches.py
+++ api/tests/test_team_matches.py
@@ -31,11 +31,12 @@ class TeamMatchesEndpointTestCase(TestCase):
         self.assertEqual(response_data['count'], matches.count(), 'expected correct number of dev team matches')
 
     def test_get_item(self):
-        match = Match.objects.get(pk=1)
-        request = factory.get('/api/teams/{0}/matches/1/'.format(self.dev_team.id))
+        match_id = 9
+        match = Match.objects.get(pk=match_id)
+        request = factory.get('/api/teams/{0}/matches/{1}/'.format(self.dev_team.id, match_id))
         force_authenticate(request, user=self.admin_user)
         view = MatchViewSet.as_view({'get': 'retrieve'})
-        response = view(request, parent_lookup_team=str(self.dev_team.id), pk='1')
+        response = view(request, parent_lookup_team=str(self.dev_team.id), pk=str(match_id))
         response.render()
         expected_data = MatchSerializer(match).data
         self.assertEqual(response.status_code, status.HTTP_200_OK, 'expected HTTP 200')
