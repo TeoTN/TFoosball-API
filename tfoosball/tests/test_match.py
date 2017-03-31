112689d2ce672c73d749abd7e9c30976226cc2b2
Zuzanna Gniewaszewska <oszka496@gmail.com>
2017-03-31 18:10:57 +0200

    Update match tests

--- tfoosball/tests/test_match.py
+++ tfoosball/tests/test_match.py
@@ -3,12 +3,23 @@ from tfoosball.models import Match, Member
 
 
 class MatchModelTest(TestCase):
-    fixtures = ['teams.json', 'players.json', 'members.json', 'matches.json']
+    fixtures = ['teams.json', 'players.json', 'members.json']
 
     def setUp(self):
         members = Member.objects.filter(team=4).order_by('-exp')[:4]
         self.members_0 = {'red_att': members[0], 'red_def': members[1], 'blue_att': members[2], 'blue_def': members[3]}
         self.members_1 = {'red_att': members[2], 'red_def': members[3], 'blue_att': members[0], 'blue_def': members[1]}
+
+    def test_below_0(self):
+        match = Match(red_score=10, blue_score=-5, **self.members_0)
+        with self.assertRaises(AssertionError):
+            match.save()
+
+    def test_above_10(self):
+        match = Match(red_score=15, blue_score=10, **self.members_0)
+        points, winner = match.calculate_points()
+        self.assertGreaterEqual(points, 0)
+        self.assertEqual(winner, 1)
 
     def test_10_0(self):
         match = Match(red_score=10, blue_score=0, **self.members_0)
@@ -26,30 +37,41 @@ class MatchModelTest(TestCase):
         self.members_0['blue_def'].exp = 1034
         self.assertEqual(self.members_0['red_att'].exp + self.members_0['red_def'].exp,
                          self.members_0['blue_att'].exp + self.members_0['blue_def'].exp)
-
-        match = Match(red_score=10, blue_score=10, **self.members_0)
-        points, winner = match.calculate_points()
-        self.assertEqual(points, 0)
-        self.assertEqual(winner, 0.5)
+        for score in range(1, 11):
+            match = Match(red_score=score, blue_score=score, **self.members_0)
+            points, winner = match.calculate_points()
+            self.assertEqual(points, 0)
+            self.assertEqual(winner, 0.5)
 
     def test_unequal_tie(self):
         self.assertNotEqual(self.members_0['red_att'].exp + self.members_0['red_def'].exp,
                             self.members_0['blue_att'].exp + self.members_0['blue_def'].exp)
 
-        match = Match(red_score=10, blue_score=10, **self.members_0)
-        points, winner = match.calculate_points()
-        self.assertEqual(winner, 0.5)
-
-    def test_tie_bias(self):
-        match = Match(red_score=10, blue_score=10, **self.members_0)
-        points_0, _ = match.calculate_points()
-        match_1 = Match(red_score=10, blue_score=10, **self.members_1)
-        points_1, _ = match_1.calculate_points()
-        self.assertEqual(points_1, -points_0)
+        for score in range(1, 11):
+            match = Match(red_score=score, blue_score=score, **self.members_0)
+            _, winner = match.calculate_points()
+            self.assertEqual(winner, 0.5)
 
     def test_bias(self):
-        match = Match(red_score=10, blue_score=0, **self.members_0)
-        points_0, _ = match.calculate_points()
-        match_1 = Match(red_score=0, blue_score=10, **self.members_1)
-        points_1, _ = match_1.calculate_points()
-        self.assertEqual(points_1, -points_0)
+        for score in range(1, 11):
+            match = Match(red_score=10, blue_score=score, **self.members_0)
+            points_0, _ = match.calculate_points()
+            match_1 = Match(red_score=score, blue_score=10, **self.members_1)
+            points_1, _ = match_1.calculate_points()
+            self.assertEqual(points_0, -points_1)
+
+    def test_update_exp(self):
+        match = Match(red_score=8, blue_score=10, **self.members_0)
+        exp_0 = (
+            self.members_0['red_att'].exp, self.members_0['red_def'].exp,
+            self.members_0['blue_att'].exp, self.members_0['blue_def'].exp)
+        match.save()
+        points = match.points
+        exp_1 = (
+            self.members_0['red_att'].exp, self.members_0['red_def'].exp,
+            self.members_0['blue_att'].exp, self.members_0['blue_def'].exp)
+
+        self.assertEqual(exp_0[0] + points, exp_1[0])
+        self.assertEqual(exp_0[1] + points, exp_1[1])
+        self.assertEqual(exp_0[2] - points, exp_1[2])
+        self.assertEqual(exp_0[3] - points, exp_1[3])
